76876a2717e8944bead27c0c77d95cb3d25222fe
Kevin Yaroch <kjyaroch@gmail.com>
2020-11-22 16:35:38 -0600

    Update expense categories to conform to QuickBooks

--- app/models/lockbox_transaction.rb
+++ app/models/lockbox_transaction.rb
@@ -17,17 +17,46 @@ class LockboxTransaction < ApplicationRecord
   ].freeze
   validates :balance_effect, inclusion: BALANCE_EFFECTS
 
-  EXPENSE_CATEGORIES = [
+  QUICKBOOKS_EXPENSE_CATEGORIES = [
+    # These match the categories used in QuickBooks
+    BUS_TRAVEL                  = 'bus_travel',
+    RIDESHARE_TRAVEL            = 'rideshare_travel',
+    TRAIN_TRAVEL                = 'train_travel',
+    AIR_TRAVEL                  = 'air_travel',
+    GAS_REIMBURSEMENT           = 'gas_reimbursement',
+    OTHER_CLIENT_TRAVEL_PARKING = 'other_client_travel:_parking',
+    ACCOMMODATIONS              = 'accommodations',
+    PRESCRIPTIONS               = 'prescriptions',
+    CHILDCARE_REIMBURSEMENTS    = 'childcare_reimbursements',
+    OTHER_CLIENT_SUPPLIES       = 'other_client_supplies',
+    PROCEDURES                  = 'procedures',
+    FOOD                        = 'food',
+  ].freeze
+
+  INTERNAL_EXPENSE_CATEGORIES = [
+    # Used for internal Lockbox purposes
+    ADJUSTMENT    = 'adjustment',
+    CASH_ADDITION = 'cash_addition',
+  ].freeze
+
+  LEGACY_EXPENSE_CATEGORIES = [
+    # These may appear on old records, but should not be set on any new records
     GAS                 = 'gas',
     PARKING             = 'parking',
     TRANSIT             = 'transit',
     CHILDCARE           = 'childcare',
     MEDICINE            = 'medicine',
-    FOOD                = 'food',
-    ADJUSTMENT          = 'adjustment',
-    CASH_ADDITION       = 'cash_addition',
     HOTEL_REIMBURSEMENT = 'hotel_reimbursement'
   ].freeze
+
+  SELECTABLE_EXPENSE_CATEGORIES = (
+    QUICKBOOKS_EXPENSE_CATEGORIES + INTERNAL_EXPENSE_CATEGORIES
+  ).freeze
+
+  EXPENSE_CATEGORIES = (
+    SELECTABLE_EXPENSE_CATEGORIES + LEGACY_EXPENSE_CATEGORIES
+  ).freeze
+
   validates :category, inclusion: EXPENSE_CATEGORIES
 
   def eff_date
