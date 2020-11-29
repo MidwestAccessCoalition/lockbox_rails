225588283226c57c97d6649888bc67ae62f3a18a
Kevin Yaroch <kjyaroch@gmail.com>
2020-11-29 17:45:59 -0600

    Add redacted to support_requests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_29_233712) do
+ActiveRecord::Schema.define(version: 2020_11_29_234516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2020_11_29_233712) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id"
+    t.boolean "redacted", default: false
     t.index ["lockbox_partner_id"], name: "index_support_requests_on_lockbox_partner_id"
     t.index ["user_id"], name: "index_support_requests_on_user_id"
   end
