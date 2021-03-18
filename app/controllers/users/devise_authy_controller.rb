781df847b310cc066cb9fbea7787ce944898da1f
BintLopez <5728859+BintLopez@users.noreply.github.com>
2021-03-18 19:12:30 -0400

    add routing for onboarding and mfa

--- app/controllers/users/devise_authy_controller.rb
+++ app/controllers/users/devise_authy_controller.rb
@@ -7,8 +7,11 @@ class Users::DeviseAuthyController < Devise::DeviseAuthyController
   end
 
   def after_authy_verified_path_for(resource)
-    # root_path
-    onboarding_success_path
+    if resource.sign_in_count > 1
+      root_path
+    else
+      onboarding_success_path
+    end
   end
 
   def after_authy_disabled_path_for(resource)
