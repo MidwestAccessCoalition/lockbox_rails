1d06f71af2e8a0f56ebddce3f2a21399af5bd8bf
BintLopez <5728859+BintLopez@users.noreply.github.com>
2021-07-19 21:19:10 -0400

    add state to org; beef up organizations factory

--- db/migrate/20210623010412_create_organizations.rb
+++ db/migrate/20210623010412_create_organizations.rb
@@ -5,6 +5,7 @@ class CreateOrganizations < ActiveRecord::Migration[6.1]
       t.string :phone_number
       t.string :street_address
       t.string :city
+      t.string :state
       t.string :zip_code
 
       t.timestamps
--- spec/factories/organizations.rb
+++ spec/factories/organizations.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
   factory :organization do
-    name { "MyString" }
-    phone_number { "MyString" }
-    street_address { "MyString" }
-    city { "MyString" }
-    zip_code { "MyString" }
+    name           { Faker::Creature::Bird.plausible_common_name + Faker::Company.suffix }
+    phone_number   { (2..9).to_a.sample.to_s + Faker::Base.numerify('#########') }
+    street_address { Faker::Address.street_address }
+    city           { Faker::Address.city }
+    state          { Faker::Address.state }
+    zip_code       { Faker::Address.zip_code }
   end
 end
