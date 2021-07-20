63b089f0bf0439be15b356b03f96c0a2c989dd32
BintLopez <5728859+BintLopez@users.noreply.github.com>
2021-07-20 00:31:52 -0400

    add min acceptable balance to piggybank

--- spec/models/piggybank_spec.rb
+++ spec/models/piggybank_spec.rb
@@ -2,4 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Piggybank, type: :model do
   it { is_expected.to have_many(:organizations).through(:piggybank_organizations) }
+
+  it 'sets the default min acceptable balance' do
+    piggy = Piggybank.new
+    expect(piggy.minimum_acceptable_balance_cents).to eq(Piggybank::MINIMUM_ACCEPTABLE_BALANCE.cents)
+  end
 end
